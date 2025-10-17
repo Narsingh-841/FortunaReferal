45d15fa5c82006960dd90ce78d75b0040f11a456
Narsingh-841 <narsingh@theoutsourcepro.com.au>
2025-10-17 12:51:01 +0530

    forrtua website

--- src/Layout/Mianlayout.tsx
+++ src/Layout/Mianlayout.tsx
@@ -1,23 +1,30 @@
 // src/components/Layout.tsx
 import { Outlet } from "react-router-dom";
-
 import Header from "../Components/Header";
 import Sidebar from "../Components/Sidebar";
 
 const Mainlayout = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      {/* Header - fixed at top */}
+      {/* Header - Always at the top */}
       <Header />
-
+      
+      {/* Main content area with sidebar and content */}
       <div className="flex flex-1">
-        {/* Main content area */}
-        <main className="flex-1 p-6 overflow-y-auto">
+        {/* Sidebar - Below header */}
+        <div className="hidden lg:block">
+          <Sidebar />
+        </div>
+        
+        {/* Mobile sidebar - overlay style */}
+        <div className="lg:hidden">
+          <Sidebar />
+        </div>
+        
+        {/* Main content */}
+        <main className="flex-1 p-6 overflow-y-auto lg:ml-0">
           <Outlet />
         </main>
-
-        {/* Sidebar below header (on the right side in horizontal layout) */}
-        <Sidebar />
       </div>
     </div>
   );
