ba44591b6db636bbbc96a78d3ba0a38fee5b4efc
Nitish Saini <nitish.saini.top@gmail.com>
2025-10-21 11:03:07 +0530

    referral table

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ServicesDashboard from "./ServiceDashboard/ServicesDashboard";
 import Mainlayout from "./Layout/Mianlayout";
+import ReferralTable from "./Referral/ReferralTable";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Mainlayout />}>
           {/* Nested routes rendered inside <Outlet /> */}
         <Route path="/services" element={<ServicesDashboard />}></Route>
+        <Route path="/my-referrals" element={<ReferralTable />}></Route>
           {/* Add more routes here */}
         </Route>
       </Routes>
